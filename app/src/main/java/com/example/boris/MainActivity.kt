631f0a72f79102ec30ccd853923978a0aa2c3493
Alekspa1 <apereverzev47@gmail.com>
2023-08-03 18:13:03 +0300

    Добавил базу данных

--- app/src/main/java/com/example/boris/MainActivity.kt
+++ app/src/main/java/com/example/boris/MainActivity.kt
@@ -4,38 +4,63 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.boris.databinding.ActivityMainBinding
+import com.firebase.ui.auth.AuthUI
+import com.firebase.ui.auth.FirebaseAuthUIActivityResultContract
+import com.firebase.ui.auth.data.model.FirebaseAuthUIAuthenticationResult
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
 import com.squareup.picasso.Picasso
 
 class MainActivity : AppCompatActivity() {
+    private val signInLauncher = registerForActivityResult(
+        FirebaseAuthUIActivityResultContract(),
+    ) { res ->
+        this.onSignInResult(res)
+    }
+    private lateinit var database: DatabaseReference
+
+    val providers = arrayListOf(
+        AuthUI.IdpConfig.EmailBuilder().build()
+    )
     private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         binding = ActivityMainBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+        database = Firebase.database.reference
         binding.apply { // тут внутри байдинга
-            edAv.setOnClickListener {
-                startActivity(Intent(this@MainActivity, RegisterActivity::class.java))
+            edAv.setOnClickListener { // кнопка
+                val signInIntent = AuthUI.getInstance()
+                    .createSignInIntentBuilder()
+                    .setAvailableProviders(providers)
+                    .build()
+                signInLauncher.launch(signInIntent)
             }
             Picasso.get()
                 .load("https://i.pinimg.com/originals/30/cc/26/30cc261ff359f3d516afbc5287245b3c.jpg")
                 .into(imView)
-        } // байдинг кончается
+        }
 
+        } // onCreate
 
+    private fun onSignInResult(result: FirebaseAuthUIAuthenticationResult) {
+        val response = result.idpResponse
+        if (result.resultCode == RESULT_OK) {
 
+            val authUser = FirebaseAuth.getInstance().currentUser
+            val email = authUser?.email.toString()
+            val uid = authUser?.displayName.toString()
+            val fireBaseUser = User(email, uid)
 
+            database.child("users").child(authUser?.uid.toString()).setValue(fireBaseUser)
+        } else {
+            // Sign in failed. If response is null the user canceled the
+            // sign-in flow using the back button. Otherwise check
+            // response.getError().getErrorCode() and handle the error.
+            // ...
+        }
+    }
 
-
-
-
-
-
-
-
-
-
-
-
-
-    } // заканчивается onCreate
-} // заканчивается класс Активити
+} // заканчивается onCreate
---
+++ app/src/main/java/com/example/boris/User.kt
@@ -0,0 +1,3 @@
+package com.example.boris
+
+data class User(val email: String, val uid: String)
